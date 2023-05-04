d6ad0a32dfe8f626d92859527353c77cf9cfad11
Peter Benc <peter.benc1@gmail.com>
2023-05-04 14:35:48 +0200

    Copy paste web3Auth connection

---
+++ src/web3auth/types.ts
@@ -0,0 +1,29 @@
+import type {OPENLOGIN_NETWORK_TYPE, LOGIN_PROVIDER} from '@toruslabs/openlogin'
+
+type ValueOf<T> = T[keyof T]
+
+export const WEB3_AUTH_NETWORKS = ['mainnet', 'testnet'] as const
+
+export type Web3AuthNetworkType = Extract<
+  OPENLOGIN_NETWORK_TYPE,
+  (typeof WEB3_AUTH_NETWORKS)[number]
+>
+
+export const WEB3_AUTH_LOGIN_PROVIDERS = [
+  'google',
+  'facebook',
+  // 'twitter',
+  // 'reddit',
+  // 'discord',
+  // 'twitch',
+  // 'apple',
+  // 'github',
+  // 'linkedin',
+  // 'line',
+  // 'wechat',
+] as const
+
+export type Web3AuthLoginProvider = Extract<
+  ValueOf<typeof LOGIN_PROVIDER>,
+  (typeof WEB3_AUTH_LOGIN_PROVIDERS)[number]
+>
