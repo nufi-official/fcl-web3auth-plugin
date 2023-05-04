b31c2d628e08543fbbda22f2158a134345e5d48d
Peter Benc <peter.benc1@gmail.com>
2023-05-04 16:39:11 +0200

    Append login modal

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import {Web3AuthLoginProvider} from './web3auth/types'
+
+export type Web3AuthProviderMetadata = {
+  name: string
+  icon: string
+  description: string
+  id: Web3AuthLoginProvider
+}
