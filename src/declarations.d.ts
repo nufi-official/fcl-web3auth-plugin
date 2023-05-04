fbba0f0ea37df3df315a268e8db27762635ac3b7
Peter Benc <peter.benc1@gmail.com>
2023-05-04 15:03:38 +0200

    Add declarations

---
+++ src/declarations.d.ts
@@ -0,0 +1,42 @@
+declare module '@onflow/fcl' {
+  export function config(settings: Record<string, unknown>): void
+  export async function authenticate(args?: {
+    service?: unknown
+  }): Promise<unknown>
+  export function withPrefix(address: string): string
+  export async function account(address: string): Promise<{
+    address: string
+    balance: number
+    code: string
+    contracts: unknown
+    keys: {
+      index: number
+      publicKey: string
+      signAlgo: number
+      hashAlgo: number
+      weight: number
+      sequenceNumber: number
+      revoked: boolean
+    }[]
+  }>
+  export function currentUser(): {snapshot: () => Promise<{addr: string}>}
+  export function unauthenticate()
+
+  export const WalletUtils: {
+    injectExtService(service: unknown): void
+    encodeMessageFromSignable(signable: unknown, signerAddress: unknown): string
+    ({address, nonce, appIdentifier}, includeDomainTag = true)
+    encodeAccountProof(
+      {
+        address,
+        nonce,
+        appIdentifier,
+      }: {address: string; nonce: string; appIdentifier: string},
+      includeDomainTag = true,
+    )
+  }
+}
+
+declare module 'sha2' {
+  export function SHA256(input: Buffer): Buffer
+}
