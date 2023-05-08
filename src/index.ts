2b0bf21e180f4c306d2831b373f60a2f5742299b
Peter Benc <peter.benc1@gmail.com>
2023-05-08 16:12:20 +0200

    Auth through modal or directly with login provider
    Export login providers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,59 @@
 import {Web3AuthLoginProvider, Web3AuthNetworkType} from './web3auth/types'
 import web3AuthProvider from './web3auth'
 import {
+  web3AuthFclServices,
+  web3AuthNetworkToFlowportApiMapping,
   web3AuthProviderMetadata,
 } from './constants'
+import {FlowportApiConnection} from './flowportApi'
+import {appendLoginModal} from './modal'
 import {Web3AuthConnection} from './web3auth/connection'
+import {Wallet} from './wallet'
+import {createApi} from './connector/api'
+import {listenToMessages} from './connector'
+import * as fcl from '@onflow/fcl'
+import {serviceDefinition} from './connector/serviceDefinition'
 
 type ConfigArgs = {clientId: string; networkType: Web3AuthNetworkType}
 
 export function config({clientId, networkType}: ConfigArgs) {
   web3AuthProvider.create(new Web3AuthConnection(networkType, clientId))
+  const wallet = new Wallet(
+    web3AuthProvider.instance(),
+    new FlowportApiConnection(web3AuthNetworkToFlowportApiMapping[networkType]),
+  )
+  const api = createApi(wallet, web3AuthFclServices)
+  listenToMessages(api)
 }
+
+async function authWithProvider(loginProvider: Web3AuthLoginProvider) {
+  await fcl.authenticate({
+    service: serviceDefinition(web3AuthFclServices[loginProvider]),
+  })
+}
+
+type AuthArgs =
+  | {
+      loginProvider: Web3AuthLoginProvider
+    }
+  | {
+      loginProviderWhiteList?: Web3AuthLoginProvider[]
+    }
+
+export async function auth(args?: AuthArgs) {
+  if (args && 'loginProvider' in args) {
+    await authWithProvider(args.loginProvider)
+    return
+  }
+  if (args && 'loginProviderWhiteList' in args) {
+    const whitelist = args.loginProviderWhiteList
+    const web3AuthProviderMetadataWhitelist = whitelist
+      ? web3AuthProviderMetadata.filter(({id}) => whitelist.includes(id))
+      : web3AuthProviderMetadata
+    appendLoginModal(web3AuthProviderMetadataWhitelist, authWithProvider)
+    return
+  }
+  appendLoginModal(web3AuthProviderMetadata, authWithProvider)
+}
+
+export {web3AuthProviderMetadata as loginProviders} from './constants'
