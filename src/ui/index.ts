641430a21a88948ad4820d57321a08210e8f7392
Peter Benc <peter.benc1@gmail.com>
2023-05-16 23:41:25 +0200

    Refactor and create UI loading overlays, confirm popup

---
+++ src/ui/index.ts
@@ -0,0 +1,70 @@
+import {createConfirmPopup} from './confirmPopup'
+import {createLoadingSpinner} from './loadingSpinner'
+import {CreateModalArgs, createLoginModal} from './modal'
+import {SigningMetadata} from './types'
+
+const overlayInnerHtml = `
+<div>
+  <div style="position: fixed;left: 0px;top: 0px;width: 100vw;height: 100vh;z-index: 1000;background: rgba(0, 0, 0, 0.16);backdrop-filter: blur(6px);"></div>
+  <div>
+    <div class="overlay-body" style="display: flex;width: 100vw;height: 100%;position: fixed;left: 0px;top: 0px;z-index: 1001;-webkit-box-pack: center;justify-content: center;-webkit-box-align: center;align-items: center;overflow: auto;overscroll-behavior-y: none;" tabindex="-1">
+    </div>
+  </div>
+</div>
+`
+
+const createOverlayElement = () => {
+  const overlay = window.document.createElement('div')
+  overlay.innerHTML = overlayInnerHtml
+
+  return {
+    close: () => overlay.remove(),
+    show: () => window.document.body.append(overlay),
+    replaceBody: (node: HTMLElement) =>
+      overlay.getElementsByClassName('overlay-body')[0].replaceChildren(node),
+  }
+}
+
+export const getUi = () => {
+  const overlay = createOverlayElement()
+
+  const showLoginModal = (
+    args: Omit<CreateModalArgs, 'onClose' | 'onLogin'>,
+  ) => {
+    overlay.show()
+    const onLogin = () => {
+      const loadingSpinner = createLoadingSpinner('Logging in...')
+      overlay.replaceBody(loadingSpinner)
+    }
+    overlay.replaceBody(
+      createLoginModal({...args, onClose: overlay.close, onLogin}),
+    )
+  }
+
+  const showLoading = (loadingText: string) => {
+    overlay.show()
+    const loadingSpinner = createLoadingSpinner(loadingText)
+    overlay.replaceBody(loadingSpinner)
+  }
+
+  const confirmSign = (
+    onSign: () => Promise<string>,
+    metadata: SigningMetadata,
+  ) => {
+    overlay.show()
+    const {popup, confirmPromise} = createConfirmPopup({
+      metadata,
+      onSign,
+      onClose: overlay.close,
+    })
+    overlay.replaceBody(popup)
+    return confirmPromise
+  }
+
+  return {
+    showLoginModal,
+    showLoading,
+    close: overlay.close,
+    confirmSign,
+  }
+}
---
+++ src/ui/types.ts
@@ -0,0 +1,1 @@
+export type SigningMetadata = {type: 'message' | 'tx'}
